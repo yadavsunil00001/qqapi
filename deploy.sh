e2eedfde7fb7c814308b25fd77f24d119af8c332
Manjesh Vinayaka <manjesh@quetzal.in>
2016-02-17 12:32:26 +0530

    Circle CI Staging Settings

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,22 @@
 #! /bin/bash
 
+echo "Changing Directory"
 cd dist
+
+echo "Git Init"
 git init
+
+echo "Adding files to git"
 git add *
+
+echo "Commiting..."
 git commit -m "new"
+
+echo "added remote"
 git remote add dokku dokku@cloude.quezx.com:partner
+
+echo "Started deploying"
 git push dokku master
+
+echo "Deployed Successfully!"
+exit 0
