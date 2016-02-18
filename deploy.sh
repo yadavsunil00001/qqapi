7b838104843c02b28f10d3a45922cfde321f7ee1
Manjesh Vinayaka <manjesh@quetzal.in>
2016-02-18 11:36:16 +0530

    Deply Config

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ echo "Git Init"
 git init
 
 echo "Adding remote"
-git remote add dokku dokku@cloud.quezx.com:partner
+git remote add dokku dokku@cloud.quezx.com:api
 
 echo "Pulling from dokku"
 git pull origin master
